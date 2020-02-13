4555087a24e0daa93e636946248ea1c6106d2667
finch <scott@mobilestyles.com>
2020-02-13 15:05:58 -0800

    added some stuff

---
+++ app/src/main/java/com/example/krakg/services/TestService.kt
@@ -0,0 +1,49 @@
+package com.example.krakg.services
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import com.example.krakg.DataManager
+
+/**
+ * A constructor is required, and must call the super [android.app.IntentService.IntentService]
+ * constructor with a name for the worker thread.
+ */
+class TestService : IntentService("TestService") {
+
+    private val TAG = "TestService"
+    val handler = Handler()
+    val runnableCode = object : Runnable {
+        override fun run() {
+            DataManager.retrofitInterface.getServerTime()
+            handler.postDelayed(this, 5000)
+        }
+    }
+
+    /**
+     * The IntentService calls this method from the default worker thread with
+     * the intent that started the service. When this method returns, IntentService
+     * stops the service, as appropriate.
+     */
+    override fun onHandleIntent(intent: Intent?) {
+        // Normally we would do some work here, like download a file.
+        // For our sample, we just sleep for 5 seconds.
+        try {
+            handler.post(runnableCode)
+        } catch (e: InterruptedException) {
+            // Restore interrupt status.mm
+            Thread.currentThread().interrupt()
+        }
+    }
+
+
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, TestService::class.java)
+        }
+
+    }
+}
+
--- app/src/main/java/com/example/krakg/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/krakg/ui/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.krakg.ui.activities
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setCustomView(R.layout.viewgroup_actionbar_bots)
+
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
