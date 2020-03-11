f3b687c3d4731ce64384869457225428f970c4c2
scott <remsyk@gmail.com>
2020-03-11 13:47:42 -0700

    new design route for application

--- app/src/main/java/com/example/krakg/adapters/ConditionAdapter.kt
+++ app/src/main/java/com/example/krakg/adapters/ConditionAdapter.kt
@@ -1,24 +1,19 @@
 package com.example.krakg.adapters
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.recyclerview.widget.RecyclerView
-import com.anychart.AnyChartView
-import com.example.krakg.ui.fragments.LineChartActivity
 import com.example.krakg.R
-import com.example.krakg.ui.activities.BotExpandedActivity
-import com.google.android.material.textview.MaterialTextView
-import kotlinx.android.synthetic.main.cardview_bot.view.*
 import kotlinx.android.synthetic.main.cardview_condition.view.*
-import kotlinx.android.synthetic.main.viewgroup_cardview_bot_bottom.view.*
 
 class ConditionAdapter(private val data: List<String>, private val context: Context): RecyclerView.Adapter<ConditionAdapter.ViewHolder>() {
+
+    private val buySellList = listOf("Title One", "Title Two", "Title Three", "Title Four")
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -35,13 +30,11 @@ class ConditionAdapter(private val data: List<String>, private val context: Cont
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         with(viewHolder) {
-            title.text = "Buy"
+
         }
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val title: MaterialTextView = view.textview_title_request
-
     }
 }
 
